ec7c08e233abbe15484cafd6fb5362aeed538d8f
zeroce <zx79239841@163.com>
2019-09-21 10:23:05 +0800

    connect to database and add sql ddl

--- README.md
+++ README.md
@@ -2,13 +2,30 @@
 没什么，想看源码可以去 github 上的 community 仓库。
 
 ## 资料
-[Spring 文档](https://spring.io/guides)  
+[Spring 指导](https://spring.io/guides)  
 [Spring Thymeleaf 文档](https://spring.io/guides/gs/serving-web-content/)  
 [目标社区 -- es](https://elasticsearch.cn/explore)  
 [用 GithubAPI 登录](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)  
 [Bootstrap 文档](https://v3.bootcss.com/components/#navbar-buttons)
+[Spring Boot 中文文档](https://www.springcloud.cc/spring-boot.html#_learning_about_spring_boot_features)  
+[Spring 官方文档2.0.0.RC1 版](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/)
+
 
 ## 工具
 [GIt](https://git-scm.com/downloads)  
 [visual-paradigm](https://online.visual-paradigm.com/cn/)  
+[H2 数据库](https://www.h2database.com/html/quickstart.html)(发现不是很好用，就换了 MySQL)
 
+## 数据库脚本
+```sql
+create table user
+(
+	id int auto_increment
+		primary key,
+	account_id varchar(100) null,
+	name varchar(100) null,
+	token char(36) null,
+	gmt_create bigint null,
+	gmt_modified bigint null
+);
+```
--- src/main/java/life/maijiang/community/controller/AuthorizeController.java
+++ src/main/java/life/maijiang/community/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package life.maijiang.community.controller;
 
 import life.maijiang.community.dto.AccessTokenDTO;
 import life.maijiang.community.dto.GithubUser;
+import life.maijiang.community.mapper.UserMapper;
+import life.maijiang.community.model.User;
 import life.maijiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,9 +29,13 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -34,8 +43,23 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (null != githubUser) {
+            // 登录成功，写 cookie 和 session
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            System.out.println("--------------------");
+            userMapper.insert(user);
+            System.out.println("+++++++++++++++++++");
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/index";
+        }else {
+            // 登录失败，重新登录
+            return "redirect:/index";
+        }
     }
 }
