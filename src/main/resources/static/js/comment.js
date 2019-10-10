c2f938ac0d446afb7c5eb7a0051d2280c9003189
zeroce <zx79239841@163.com>
2019-10-10 14:49:26 +0800

    add asynchronous login function(incompletely)

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -1,20 +1,27 @@
 function postComment() {
     $.ajax({
-        type:"POST",
-        url:"/comment",
-        contentType:"application/json",
+        type: "POST",
+        url: "/comment",
+        contentType: "application/json",
         data: JSON.stringify({
             "parentId": $("#questionId").val(),
             "content": $("#comment").val(),
             "type": 1
         }),
-        success:function(response) {
+        success: function (response) {
             if (response.code == 200) {
 
             } else {
-                alert(response.message);
+                if (response.code == 2003) {
+                    if (confirm(response.message)) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=6b94612764d44e4ce138&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", "true");
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
         },
-        dataType:"json"
+        dataType: "json"
     });
 }
